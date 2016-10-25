e30f0104ebd4c4edae53097c1ac1291af989a02c
lin onetwo <lindongwu11@gmail.com>
2016-10-26 00:04:43 +0800

    add es6

--- src/index.js
+++ src/index.js
@@ -1,16 +1,70 @@
-'use strict';
+
+const supportedColors = {
+  r: 'r',
+  g: 'g',
+  b: 'b'
+};
+
+type SupportedColorsType = $Keys<typeof supportedColors>;
+
+function decorateBlinkInterval(target, key, descriptor) {
+  const method = descriptor.value;
+  const moreInterval = 100;
+  let ret;
+  descriptor.value = (...args) => {
+    args[0] += moreInterval;
+    console.log(args);
+    ret = method.apply(target, args);
+    return ret;
+  };
+  return descriptor;
+}
+
+class Blinker {
+  constructor(blinkLength: number = 100, blueBlinkInterval: number = 200, redBlinkInterval: number = 600, greenBlinkInterval: number = 1000) {
+    this.started = false;
+    this.blinkLength = blinkLength;
+    this.init(blueBlinkInterval, redBlinkInterval, greenBlinkInterval);
+  }
+
+  @decorateBlinkInterval
+  init(blueBlinkInterval: number, redBlinkInterval: number, greenBlinkInterval: number) {
+    this.blueBlinkInterval = blueBlinkInterval;
+    this.redBlinkInterval = redBlinkInterval;
+    this.greenBlinkInterval = greenBlinkInterval;
+  }
+
+  async blink(color: SupportedColorsType) {
+    while(true) {
+      async () => {
+        $(`#led-${color}`).turnOn();
+        await Promise.delay(this.blinkLength);
+        $(`#led-${color}`).turnOff();
+        await Promise.delay(this.blueBlinkInterval);
+      }
+    }
+  }
+
+  startBlink() {
+    if (this.started) {
+      return;
+    }
+    this.started = true;
+    ['r', 'g', 'b'].map(color => this.blink(color));
+  }
+}
 
 $.ready(function (error) {
-    if (error) {
-        console.log(error);
-        return;
-    }
+  if (error) {
+    console.log(error);
+    return;
+  }
 
-    $('#led-r').turnOn();
-    $('#led-g').turnOn();
+  $('#led-b').turnOn();
+  $('#led-g').turnOn();
 });
 
 $.end(function () {
-    $('#led-r').turnOff();
-    $('#led-g').turnOff();
+  $('#led-b').turnOff();
+  $('#led-g').turnOff();
 });
